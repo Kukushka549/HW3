3e486033cd5d2893174be68796537d483b8a491e
Kukushka549 <sviridovalexey073@gmail.com>
2025-04-28 01:00:10 +0300

    HW3

--- src/Main.java
+++ src/Main.java
@@ -44,19 +44,19 @@ public class Main {
         int february1 = (int) day * 28;
         int february2 = (int) day * 29;
 
-        System.out.println("За " + twentyMinutes + " минут машина произвела " + (efficiency*time*20) + "бутылок. ");
+        System.out.println("За " + twentyMinutes + " минут машина произвела " + (efficiency*time*20) + " бутылок. ");
         System.out.println("В одном дне " + day + " минут. ");
-        System.out.println("За " + day + " минут машина произвела " + (efficiency*day) + "бутылок. ");
+        System.out.println("За " + day + " минут машина произвела " + (efficiency*day) + " бутылок. ");
         System.out.println("В трёх днях " + threeDays + " минут. ");
-        System.out.println("За " + threeDays + " минут машина произвела " + (efficiency*threeDays) + "бутылок. ");
+        System.out.println("За " + threeDays + " минут машина произвела " + (efficiency*threeDays) + " бутылок. ");
         System.out.println("Если в месяце 30 дней, то в нём " + month1 + " минут. ");
-        System.out.println("За " + month1 + " минут машина произвела " + (efficiency*month1) + "бутылок. ");
+        System.out.println("За " + month1 + " минут машина произвела " + (efficiency*month1) + " бутылок. ");
         System.out.println("Если в месяце 31 день, то в нём " + month2 + " минут. ");
-        System.out.println("За " + month2 + " минут машина произвела " + (efficiency*month2) + "бутылок. ");
+        System.out.println("За " + month2 + " минут машина произвела " + (efficiency*month2) + " бутылок. ");
         System.out.println("Если это февраль с 28-ю днями, то в нём " + february1 + " минут. ");
-        System.out.println("За " + february1 + " минут машина произвела " + (efficiency*february1) + "бутылок. ");
+        System.out.println("За " + february1 + " минут машина произвела " + (efficiency*february1) + " бутылок. ");
         System.out.println("Если это февраль с 29-ю днями, то в нём " + february2 + " минут. ");
-        System.out.println("За " + february2 + " минут машина произвела " + (efficiency*february2) + "бутылок. ");
+        System.out.println("За " + february2 + " минут машина произвела " + (efficiency*february2) + " бутылок. ");
 
         byte allPrint = 120;
         byte brownPrint = 4;
@@ -68,6 +68,46 @@ public class Main {
 
         System.out.println("В школе, где " + classes + " классов, нужно " + allWhitePrint + " банок белой краски и " + allBrownPrint + " банок коричневой краски. ");
 
+        byte banana = 80;
+        byte milk = 105;
+        byte iceCream = 100;
+        byte egg = 70;
 
+        int milkshake = (short) banana*5 + milk*2 + iceCream*2 + egg *4;
+        float milkshakeKg = (float) milkshake/1000;
+
+        System.out.println("Милкшейк данного спортсмена будет весить " + milkshake + " грамм, или " + milkshakeKg + " кг. ");
+
+        short weight = 7000;
+        short ration1 = 250;
+        short ration2 = 500;
+
+        int result1 = weight/ration1;
+        int result2 = weight/ration2;
+        int average = (byte) (result1+result2)/2;
+
+        System.out.println("При похудении по " + ration1 + " грамм в день понадобится " + result1 + " дней. ");
+        System.out.println("При похудении по " + ration2 + " грамм в день понадобится " + result2 + " дней. ");
+        System.out.println("В среднем, на похудение уйдёт " + average + " дней.");
+
+        int masha = 67760;
+        int denis = 83690;
+        int kris = 76230;
+
+        int mashaYear = masha*12;
+        int denisYear = denis*12;
+        int krisYear = kris*12;
+
+        int mashaNew = masha/100*110;
+        int denisNew = denis/100*110;
+        int krisNew = kris/100*110;
+
+        int mashaYearNew = mashaYear/100*110;
+        int densYearNew = denisYear/100*110;
+        int krisYearNew = krisYear/100*110;
+
+        System.out.println("Маша теперь получает " + mashaNew + " рублей. Разница в годовом доходе составила " + (mashaYearNew-mashaYear) + " рублей. ");
+        System.out.println("Денис теперь получает " + denisNew + " рублей. Разница в годовом доходе составила " + (densYearNew-denisYear) + " рублей. ");
+        System.out.println("Кристина теперь получает " + krisNew + " рублей. Разница в годовом доходе составила " + (krisYearNew-krisYear) + " рублей. ");
     }
 }
